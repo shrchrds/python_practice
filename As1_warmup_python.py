ca0020acc709913748ea0d3241915f9796f23e01
Shivanand <datasnekatpure@gmail.com>
2022-10-19 19:10:43 +0530

    first commit

---
+++ As1_warmup_python.py
@@ -0,0 +1,45 @@
+# 1. Write a command to get the Python version you are using
+import sys
+print(sys.version)
+
+# 2. Write a python script to print Hello Python on the screen directly in the command line.
+# go to command prompt > type python > type print("Hello Python")
+
+# 3. Write a python script and store it in a file and execute the file from the command line.
+print("Python script from command line")
+# go to command prompt, enter python filename  and hit enter
+
+# 4. Write a python script to print Hello Python on the screen using visual studio code.
+print("Hello Python")
+# run file or go to terminal> type python filename > Hit Enter
+
+# 5. Write a python script to print Hello on the first line and Python on the second line
+print("Hello \nPython")
+
+# 6. Write a python script to print “MySirG” on the screen
+print("MySirG")
+
+# 7. Write a python script to print “Teacher’s Day” on the screen
+print("Teacher's Day")
+
+# 8. Write a python script to print the value of a variable. Variable contains your name as data.
+data = 'Shivanand'
+print(data)
+
+# 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name = input("Enter your name: ")
+age = input("Enter your Age: ")
+qualification = input("Enter your qualification: ")
+yoe = input("Enter your years of experience: ")
+print(name)
+print(age)
+print(qualification)
+print(yoe)
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+var1 = int(input("Enter a number1: "))
+var2 = int(input("Enter a number2: "))
+var3 = int(input("Enter a number3: "))
+print(var1)
+print(var2)
+print(var3)
